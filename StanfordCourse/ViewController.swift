d83fb9ddeefe5d421d02e1b0e716ec66e1922253
Timur Cheberda <likedune@gmail.com>
2019-03-15 16:17:15 +0300

    Initial Commit

---
+++ StanfordCourse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StanfordCourse
+//
+//  Created by Тимур Чеберда on 15/03/2019.
+//  Copyright © 2019 Tmur Cheberda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
